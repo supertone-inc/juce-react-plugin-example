c8cc3e19c340fa7eb50db1293e3febfc5508c262
krazijames <krazijames@gmail.com>
2021-11-23 19:28:48 +0900

    Cleanup renderer

--- renderer/src/App.js
+++ renderer/src/App.js
@@ -1,28 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
 const websocketPort = new URL(window.location.href).searchParams.get("port");
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>WebSocket Port: {websocketPort}</p>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      WebSocket Port: {websocketPort}
     </div>
   );
 }
-
-export default App;
