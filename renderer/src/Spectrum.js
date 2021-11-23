9ac847f213deb0ce9f972916f8a03e903f1984fe
krazijames <krazijames@gmail.com>
2021-11-24 03:32:08 +0900

    Convert X coordinate to dB scale

--- renderer/src/Spectrum.js
+++ renderer/src/Spectrum.js
@@ -17,11 +17,18 @@ export default function Spectrum({ spectrum, style, ...props }) {
 
     canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const barWidth = canvas.width / spectrum.length;
+    const scopeSize = spectrum.length / 4;
+    const barWidth = canvas.width / scopeSize;
     canvasCtx.fillStyle = "#f00";
 
-    for (var i = 0, x = 0; i < spectrum.length; i++, x += barWidth + 1) {
-      const barHeight = canvas.height * spectrum[i];
+    for (var i = 0, x = 0; i < scopeSize; i++, x += barWidth + 1) {
+      const skewedProportionX = 1 - Math.exp(Math.log(1 - i / scopeSize) * 0.2);
+      const dBIndex = Math.min(
+        spectrum.length,
+        (skewedProportionX * spectrum.length).toFixed()
+      );
+
+      const barHeight = canvas.height * spectrum[dBIndex];
 
       canvasCtx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
     }
