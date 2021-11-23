6d402962dbc19f68164a80289cf03e720676de48
krazijames <krazijames@gmail.com>
2021-11-23 14:29:14 +0900

    Format
    `clang-format ./main/*.h ./main/*.cpp -i --fallback-style=Microsoft`

--- main/PluginEditor.h
+++ main/PluginEditor.h
@@ -4,22 +4,22 @@
 #include <juce_gui_extra/juce_gui_extra.h>
 
 //==============================================================================
-class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
+class AudioPluginAudioProcessorEditor : public juce::AudioProcessorEditor
 {
-public:
-    explicit AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor&);
+  public:
+    explicit AudioPluginAudioProcessorEditor(AudioPluginAudioProcessor &);
     ~AudioPluginAudioProcessorEditor() override;
 
     //==============================================================================
-    void paint (juce::Graphics&) override;
+    void paint(juce::Graphics &) override;
     void resized() override;
 
-private:
+  private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
-    AudioPluginAudioProcessor& processorRef;
+    AudioPluginAudioProcessor &processorRef;
 
     juce::WebBrowserComponent browser;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(AudioPluginAudioProcessorEditor)
 };
