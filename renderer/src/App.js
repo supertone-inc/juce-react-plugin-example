e6f0942eeaef918cf0d1e93773486444cecb2ef9
krazijames <krazijames@gmail.com>
2021-11-24 00:48:36 +0900

    Adjust text style

--- renderer/src/App.js
+++ renderer/src/App.js
@@ -2,9 +2,9 @@ import Level from "./Level";
 import Spectrum from "./Spectrum";
 import { createWebSocketStore, StoreProvider } from "./store";
 
-const websocketPort = new URL(window.location.href).searchParams.get("port");
+const port = new URL(window.location.href).searchParams.get("port");
 
-const store = createWebSocketStore(`ws://localhost:${websocketPort}`);
+const store = createWebSocketStore(`ws://localhost:${port}`);
 
 export default function App() {
   return (
@@ -20,8 +20,8 @@ export default function App() {
           <Spectrum style={{ flex: 1 }} spectrum={state?.spectrum} />
           <Level style={{ width: 20 }} level={state?.level} />
 
-          <div style={{ position: "fixed", left: 0, bottom: 0, margin: 10 }}>
-            WebSocket Port: {websocketPort}
+          <div style={{ position: "fixed", left: 0, bottom: 0, margin: 10, fontSize: 10 }}>
+            Port: {port}
           </div>
         </div>
       )}
