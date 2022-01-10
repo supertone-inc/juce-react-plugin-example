75ed6db4f68a7b52faf36caf5ac7e996307dfeac
krazijames <krazijames@gmail.com>
2022-01-10 16:27:47 +0900

    Use `FetchContent` instead of submodules

--- main/dependencies.cmake
+++ main/dependencies.cmake
@@ -1,26 +1,81 @@
-include_directories(SYSTEM deps/boost/asio/include)
-include_directories(SYSTEM deps/boost/assert/include)
-include_directories(SYSTEM deps/boost/bind/include)
-include_directories(SYSTEM deps/boost/config/include)
-include_directories(SYSTEM deps/boost/core/include)
-include_directories(SYSTEM deps/boost/date_time/include)
-include_directories(SYSTEM deps/boost/hana/include)
-include_directories(SYSTEM deps/boost/intrusive/include)
-include_directories(SYSTEM deps/boost/move/include)
-include_directories(SYSTEM deps/boost/mpl/include)
-include_directories(SYSTEM deps/boost/numeric_conversion/include)
-include_directories(SYSTEM deps/boost/predef/include)
-include_directories(SYSTEM deps/boost/preprocessor/include)
-include_directories(SYSTEM deps/boost/regex/include)
-include_directories(SYSTEM deps/boost/smart_ptr/include)
-include_directories(SYSTEM deps/boost/static_assert/include)
-include_directories(SYSTEM deps/boost/system/include)
-include_directories(SYSTEM deps/boost/throw_exception/include)
-include_directories(SYSTEM deps/boost/utility/include)
-include_directories(SYSTEM deps/boost/type_traits/include)
-include_directories(SYSTEM deps/json/single_include)
-include_directories(SYSTEM deps/lager)
-include_directories(SYSTEM deps/websocketpp)
-include_directories(SYSTEM deps/zug)
+include(FetchContent)
+set(FETCHCONTENT_QUIET OFF)
 
-add_subdirectory(deps/JUCE)
+set(BOOST_TAG boost-1.78.0)
+set(BOOST_LIBS
+    asio
+    assert
+    bind
+    config
+    core
+    date_time
+    hana
+    intrusive
+    move
+    mpl
+    numeric_conversion
+    predef
+    preprocessor
+    regex
+    smart_ptr
+    static_assert
+    system
+    throw_exception
+    utility
+    type_traits
+)
+foreach(lib IN LISTS BOOST_LIBS)
+    FetchContent_Declare(
+        boost_${lib}
+        GIT_REPOSITORY  https://github.com/boostorg/${lib}.git
+        GIT_TAG         ${BOOST_TAG}
+        GIT_SHALLOW     TRUE
+    )
+    FetchContent_Populate(boost_${lib})
+    include_directories(SYSTEM ${boost_${lib}_SOURCE_DIR}/include)
+endforeach()
+
+FetchContent_Declare(
+    json
+    GIT_REPOSITORY  https://github.com/nlohmann/json.git
+    GIT_TAG         v3.10.4
+    GIT_SHALLOW     TRUE
+)
+FetchContent_Populate(json)
+include_directories(SYSTEM ${json_SOURCE_DIR}/single_include)
+
+FetchContent_Declare(
+    JUCE
+    GIT_REPOSITORY  https://github.com/juce-framework/JUCE.git
+    GIT_TAG         6.1.2
+    GIT_SHALLOW     TRUE
+)
+FetchContent_Populate(JUCE)
+add_subdirectory(${juce_SOURCE_DIR} ${juce_BINARY_DIR} EXCLUDE_FROM_ALL)
+
+FetchContent_Declare(
+    lager
+    GIT_REPOSITORY  https://github.com/arximboldi/lager.git
+    GIT_TAG         c5588f04405d4d0bf85bd587dd90516b24b1ec65
+    GIT_SHALLOW     TRUE
+)
+FetchContent_Populate(lager)
+include_directories(SYSTEM ${lager_SOURCE_DIR})
+
+FetchContent_Declare(
+    websocketpp
+    GIT_REPOSITORY  https://github.com/zaphoyd/websocketpp.git
+    GIT_TAG         0.8.2
+    GIT_SHALLOW     TRUE
+)
+FetchContent_Populate(websocketpp)
+include_directories(SYSTEM ${websocketpp_SOURCE_DIR})
+
+FetchContent_Declare(
+    zug
+    GIT_REPOSITORY  https://github.com/arximboldi/zug.git
+    GIT_TAG         033dadbed463ff3430b7ebc36df8a46f4f0f5078
+    GIT_SHALLOW     TRUE
+)
+FetchContent_Populate(zug)
+include_directories(SYSTEM ${zug_SOURCE_DIR})
